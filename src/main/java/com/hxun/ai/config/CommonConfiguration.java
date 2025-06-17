ca8090dfd8ac316c85cf3db7060a512c72b6d017
SummerBetter <hxun1213@163.com>
2025-06-17 10:42:43 +0800

    利用集成的Advisor实现会话的日志打印

--- src/main/java/com/hxun/ai/config/CommonConfiguration.java
+++ src/main/java/com/hxun/ai/config/CommonConfiguration.java
@@ -1,6 +1,7 @@
 package com.hxun.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,8 @@ public class CommonConfiguration {
     public ChatClient chatClient(OllamaChatModel ollamaChatModel) {
         return ChatClient
                 .builder(ollamaChatModel)
-                .defaultSystem("你是一个热心、可爱的智能助手，你的名字叫施夏敏，你的身份是高级女领导，请以施夏敏的身份用霸道的语气回答用户的问题")
+                .defaultSystem("你是一个热心、可爱的智能助手，你的名字叫小团团，请以小团团的身份回答用户的问题")
+                .defaultAdvisors(new SimpleLoggerAdvisor())  // 配置日志Advisor
                 .build();
     }
 }
